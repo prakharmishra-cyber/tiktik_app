8e78854915a613510c6b4fd52698b799f3eccd01
Prakhar Mishra <mprakhar35@gmail.com>
2022-08-15 21:23:23 +0000

    first commmit

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div className="text-3xl underline font-bold">    
+      tik tik
+    </div>
+  )
+}
+
+export default Home
